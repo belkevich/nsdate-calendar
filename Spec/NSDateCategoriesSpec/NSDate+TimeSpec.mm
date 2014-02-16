753ce48ff39bc6164753aaf4b43d7e23151ca477
Alexey Belkevich <belkevich.alexey@gmail.com>
2014-02-16 12:32:14 +0200

    Fixed formatting in time spec

--- Spec/NSDateCategoriesSpec/NSDate+TimeSpec.mm
+++ Spec/NSDateCategoriesSpec/NSDate+TimeSpec.mm
@@ -24,11 +24,9 @@ describe(@"Date with time", ^
     beforeEach(^
                {
                    formatter = [[NSDateFormatter alloc] init];
-                   [formatter setTimeZone:[NSTimeZone localTimeZone]];
                    [formatter setDateFormat:@"yyyy'-'MM'-'dd HH:mm:ss"];
                    date = [formatter dateFromString:@"2014-01-02 10:20:30"];
                });
-
 
     it(@"should get local hour", ^
     {
@@ -48,97 +46,89 @@ describe(@"Date with time", ^
     it(@"should get date with changed local hour", ^
     {
         date = [date dateBySettingHour:22];
-        date.hour should equal(22);
-        date.minute should equal(20);
-        date.second should equal(30);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(22);
+        date.minute should equal(20);
+        date.second should equal(30);
     });
 
     it(@"should get date with changed local minute", ^
     {
         date = [date dateBySettingMinute:11];
-        date.hour should equal(10);
-        date.minute should equal(11);
-        date.second should equal(30);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(10);
+        date.minute should equal(11);
+        date.second should equal(30);
     });
 
     it(@"should get date with changed local second", ^
     {
         date = [date dateBySettingSecond:33];
-        date.hour should equal(10);
-        date.minute should equal(20);
-        date.second should equal(33);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(10);
+        date.minute should equal(20);
+        date.second should equal(33);
     });
 
     it(@"should get date with changed local hour, minute, second", ^
     {
         date = [date dateBySettingHour:22 minute:11 second:33];
-        date.hour should equal(22);
-        date.minute should equal(11);
-        date.second should equal(33);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(22);
+        date.minute should equal(11);
+        date.second should equal(33);
     });
 
     it(@"should get date with added hour", ^
     {
         date = [date dateByAddingHour:1];
-        date.hour should equal(11);
-        date.minute should equal(20);
-        date.second should equal(30);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(11);
+        date.minute should equal(20);
+        date.second should equal(30);
     });
 
     it(@"should get date with added minute", ^
     {
         date = [date dateByAddingMinute:1];
-        date.hour should equal(10);
-        date.minute should equal(21);
-        date.second should equal(30);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(10);
+        date.minute should equal(21);
+        date.second should equal(30);
     });
 
     it(@"should get date with added second", ^
     {
         date = [date dateByAddingSecond:5];
-        date.hour should equal(10);
-        date.minute should equal(20);
-        date.second should equal(35);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(10);
+        date.minute should equal(20);
+        date.second should equal(35);
     });
 
     it(@"should get date with added hour, minute, second", ^
     {
         date = [date dateByAddingHour:1 minute:2 second:3];
-        date.hour should equal(11);
-        date.minute should equal(22);
-        date.second should equal(33);
-
         date.year should equal(2014);
         date.month should equal(1);
         date.day should equal(2);
+        date.hour should equal(11);
+        date.minute should equal(22);
+        date.second should equal(33);
     });
 });
 
