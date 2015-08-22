ee8b58afa609631d257d648eba9b91031ab26116
Entalpi <alexander.lingtorp@gmail.com>
2015-08-22 10:28:01 +0200

    Added NSDate extension Hour with property hourInYear.
    The property returns the number current hour in the current year.
    
    The interval is 1 to 8760 (24 * 365).

---
+++ Pod/NSDate+Hour.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+Hour.h
+//  NSDate+Calendar
+//
+//  Created by Alexander Lingtorp on 22/08/15.
+//  Copyright © 2015 Okolodev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Hour)
+
+@property (nonatomic, readonly) NSUInteger hourInYear;
+
+@end
